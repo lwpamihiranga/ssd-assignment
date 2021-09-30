cd8f9aa669172c5804a79d1fe76209affc293c1e
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-09-30 21:40:51 +0530

    init node.js backend

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// initialize env
+dotenv.config();
+
+// initialize express
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api', (req, res, next) => {
+    res.status(200).json({
+        result: {
+            message: 'API is working!',
+        },
+    });
+});
+
+app.use((req, res, next) => {
+    const error = new Error();
+    error.message = 'Route not Found!';
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+// initialize app
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on PORT: ${process.env.PORT}`);
+});
