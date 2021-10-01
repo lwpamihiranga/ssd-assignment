e88380adabe351c96bcf041ed3fd36d973a8ab31
Amith Mihiranga <lwpamihiranga@gmail.com>
2021-10-01 22:04:09 +0530

    configure route to persist tokens

--- backend/routes/google/index.js
+++ backend/routes/google/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { autthencticateController } = require('../../controllers/google');
+const {
+    autthencticateController,
+    saveContoller,
+} = require('../../controllers/google');
 
 router.get('/google/authenticate', autthencticateController);
+router.get('/google/save', saveContoller);
 
 module.exports = router;
