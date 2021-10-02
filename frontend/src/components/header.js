4450ad955264e86b5a7f7212bd7bc58bd5bbc461
RahulFernando <rahulrulz680@gmail.com>
2021-10-02 09:47:23 +0530

    style fb login button

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = ({ handleLogin }) => {
   return (
     <nav className="navbar navbar-dark bg-dark justify-content-between">
-      <div class="container-fluid">
+      <div className="container-fluid">
         <a className="navbar-brand" href="#">
           BLOG
         </a>
         <div className="cform-inline">
           <button className="fb connect" onClick={handleLogin}>
-            Authenticate with Facebook
+            Sign in with Facebook
           </button>
         </div>
       </div>
