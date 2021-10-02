ca2e604b3312e06ae0a91affb956bfb2496ae136
RahulFernando <rahulrulz680@gmail.com>
2021-10-02 09:37:22 +0530

    change nav bar color

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const Header = ({ handleLogin }) => {
-    return (
-        <nav className="navbar navbar-light bg-light justify-content-between">
-            <a className="navbar-brand" href="#">
-                BLOG
-            </a>
-            <div className="cform-inline">
-                <button className="fb connect" onClick={handleLogin}>
-                    Authenticate with Facebook
-                </button>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-dark bg-dark justify-content-between">
+      <div class="container-fluid">
+        <a className="navbar-brand" href="#">
+          BLOG
+        </a>
+        <div className="cform-inline">
+          <button className="fb connect" onClick={handleLogin}>
+            Authenticate with Facebook
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
